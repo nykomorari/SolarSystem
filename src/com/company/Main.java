a0b4a0532c5e92086f7e37771e2d2485248b2e2a
hydropandaa@gmail.com <hydropandaagmail.com@nicolaes-mbp.myfiosgateway.com>
2022-01-29 11:53:25 -0500

    Features Sun with Errors to be resolved

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,5 +7,11 @@ public class Main {
         milkyway.sun=1;
         milkyway.Moon=2;
         milkyway.Planets=3;
+
+        SUN Solana = new SUN();
+        Solana.Heat = "438783845kj";
+        Solana. radius = "4368934869348km";
+        Solana.temp = "5345834c;
+
     }
 }
---
+++ src/com/company/SUN.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class SUN {
+    String Heat;
+    String temp;
+    String radius;
+
+
+}
