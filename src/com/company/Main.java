236fa248062ada98f80ce6b646c5b7d0cca00fd6
hydropandaa@gmail.com <hydropandaagmail.com@nicolaes-mbp.myfiosgateway.com>
2022-01-29 11:04:10 -0500

    The Basic Structure is Complete

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         SolarSystem milkyway = new SolarSystem();
-        
+        milkyway.sun=1;
+        milkyway.Moon=2;
+        milkyway.Planets=3;
     }
 }
